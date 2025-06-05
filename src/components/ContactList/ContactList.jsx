484af4a397c0d7b13f72d89e313c36c3f274261e
Andrio-fav <andriy2006lukashuk@gmail.com>
2025-06-05 23:47:04 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+import { selectContacts } from '../../redux/contactsSlice.js';
+import { selectFilter } from '../../redux/filtersSlice.js';
+
+export default function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filterValue = useSelector(selectFilter);
+
+  const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filterValue.toLowerCase()));
+
+  return (
+    <ul className={css.wrapper}>
+      {filteredContacts.map((contact) => (
+        <li key={contact.id}>
+          <Contact contact={contact} />
+        </li>
+      ))}
+    </ul>
+  );
+}
